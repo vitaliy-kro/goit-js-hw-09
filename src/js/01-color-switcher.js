892cf221a0fb915585202cbe822df73e8f6ec384
vitaliy-kro <krochakv6@gmail.com>
2022-08-22 11:38:52 +0300

    finished 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+let isStarted = false;
+let backgroundChangeInterval = null;
+refs.start.addEventListener('click', onStartButtonClick);
+refs.stop.addEventListener('click', onStopButtonClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartButtonClick() {
+  if (isStarted) return;
+  backgroundChangeInterval = setInterval(changeBodyColor, 1000);
+}
+function onStopButtonClick() {
+  isStarted = false;
+  clearInterval(backgroundChangeInterval);
+}
+function changeBodyColor() {
+  isStarted = true;
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
